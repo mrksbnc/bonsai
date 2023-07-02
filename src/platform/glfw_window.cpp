244b087263dc245c654e4aafff9d79f0ffc4cf65
Bence Márkus <markus.bence@icloud.com>
2023-07-02 21:21:22 +0200

    small refactor

--- src/platform/glfw_window.cpp
+++ src/platform/glfw_window.cpp
@@ -5,38 +5,17 @@ GlfwWindow::GlfwWindow() {
 }
 
 void GlfwWindow::open() {
-	GLFWwindow* window;
-
-	/* Initialize the library */
 	if (!glfwInit())
 		return;
 
-	/* Create a windowed mode window and its OpenGL context */
-	window = glfwCreateWindow(800, 600, "Hello World", NULL, NULL);
+	_window = glfwCreateWindow(800, 600, "Bonsai", NULL, NULL);
 
-	if (!window) {
+	if (!_window) {
 		glfwTerminate();
 		return;
 	}
 
-	/* Make the window's context current */
-	glfwMakeContextCurrent(window);
-
-	/* Loop until the user closes the window */
-	while (!glfwWindowShouldClose(window)) {
-
-		/* Render here */
-		// glClear(GL_COLOR_BUFFER_BIT);
-
-		/* Swap front and back buffers */
-		glfwSwapBuffers(window);
-
-		/* Poll for and process events */
-		glfwPollEvents();
-	}
-
-	glfwTerminate();
-
+	glfwMakeContextCurrent(_window);
 	return;
 }
 
