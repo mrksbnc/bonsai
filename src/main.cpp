e3310304ca7b1f05d57def66adb27729d555d4c7
Bence Márkus <markus.bence@icloud.com>
2023-07-02 15:29:30 +0200

    glfw linked

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,8 @@
 #include <iostream>
 
 int main(void) {
+	std::cout << "Hello, World!" << std::endl;
+
 	GLFWwindow* window;
 
 	/* Initialize the library */
@@ -9,7 +11,8 @@ int main(void) {
 		return -1;
 
 	/* Create a windowed mode window and its OpenGL context */
-	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+	window = glfwCreateWindow(800, 600, "Hello World", NULL, NULL);
+
 	if (!window) {
 		glfwTerminate();
 		return -1;
@@ -20,8 +23,9 @@ int main(void) {
 
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window)) {
+
 		/* Render here */
-		glClear(GL_COLOR_BUFFER_BIT);
+		// glClear(GL_COLOR_BUFFER_BIT);
 
 		/* Swap front and back buffers */
 		glfwSwapBuffers(window);
@@ -31,5 +35,6 @@ int main(void) {
 	}
 
 	glfwTerminate();
+
 	return 0;
 }
